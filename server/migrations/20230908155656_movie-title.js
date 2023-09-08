5f744426a1c658394627d0129e56a62b4af3ab6a
DannieB23 <137961298+DannieB23@users.noreply.github.com>
2023-09-08 09:21:58 -0700

    Finished Part of Level 1 up until refactor for front-end

---
+++ server/migrations/20230908155656_movie-title.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable(
+        'movie_title', table => {
+            table.increments();
+            table.string("title", 256);
+
+        }
+    )
+
+};
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('movie_title');
+};
